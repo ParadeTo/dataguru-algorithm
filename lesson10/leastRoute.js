4aed04e3ee90e8e1ace45c8bc0aa433b5dd84384
ayou <youxingzhi@qq.com>
2017-03-04 09:29:27 +0800

    dijkstra

--- lesson10/leastRoute.js
+++ lesson10/leastRoute.js
@@ -8,9 +8,14 @@ function dijkstra(S, M) {
   let result = [], route = [];
   // 初始化
   for (let i = 0; i < vNum; i++) {
-    result[i] = Infinity;
+    if (i === 0) {
+      result[i] = 0;
+      route[i] = 0;
+    } else {
+      result[i] = Infinity;
+      route[i] = 0;
+    }
     D[i] = M[S][i];
-    route[i] = S;
   }
   // 添加剩余的点到T集合
   for (let i = 1; i < vNum; i++) {
@@ -26,7 +31,7 @@ function dijkstra(S, M) {
 
     // 更新V-T集合中各点到起点的最短距离
     for (let j = 0; j < vNum; j++) {
-      if (result[j] == Infinity && D[j] > sp + M[newPoint][j]) {
+      if (result[j] === Infinity && D[j] > sp + M[newPoint][j]) {
         D[j] = sp + M[newPoint][j];
         route[j] = newPoint;
       }
@@ -34,5 +39,27 @@ function dijkstra(S, M) {
   }
   return {result, route};
 }
+
+const M = [
+    [0,1,4,Infinity,Infinity,Infinity],
+    [1,0,1,5,Infinity,Infinity],
+    [4,1,0,2,3,Infinity],
+    [Infinity,5,2,0,Infinity,6],
+    [Infinity,Infinity,3,Infinity,0,2],
+    [Infinity,Infinity,Infinity,6,2,0],
+]
+
+const {result, route} = dijkstra(0, M);
+console.log(result, route);
+for (let i = 1, len = route.length; i < len; i++) {
+  let path = i;
+  let prevPoint = i;
+  do {
+    prevPoint = route[prevPoint];
+    path = prevPoint + '->' + path;
+  } while (prevPoint != 0);
+  console.log(path, ' ', result[i]);
+}
+
 
 
